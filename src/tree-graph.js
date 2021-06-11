97dfd1f2b6cb22f4d417edbed39fa9450af5b10a
Kevin Burkett <kevin.burkett@outlook.com>
2021-06-10 21:11:19 -0700

    Move Tree to Class

--- src/tree-graph.js
+++ src/tree-graph.js
@@ -1,9 +1,60 @@
 /*
-Undirected - Edges go both Directions
-Not Weighted - Does not have extra properties
-No Cycles - Does not reference itself
+    Undirected - Edges go both Directions
+    Not Weighted - Does not have extra properties
+    No Cycles - Does not reference itself
 */
-const airports = 'PHX,BKK,OKC,JFK,LAX,MEX,EZE,HEL,LOS,LAP,LIM'.split(',')
+class Routes {
+    constructor() {
+        this.routes = new Map()
+    }
+    addNode(airport) {
+        this.routes.set(airport, [])
+    }
+    addUndirectedEdge(origin, destination) {
+        if (!this.routes.has(origin) || !this.routes.has(destination)) return
+            this.routes.get(origin).push(destination)
+            this.routes.get(destination).push(origin)
+    }
+    findLeastHopsRoute(origin, destination) {
+        // BFS
+        const visited = new Set(), q = [origin]
+        let steps = 1
+        while (q.length > 0) {
+            const airport = q.pop(), routes = this.routes.get(airport)
+            for (const route of routes) {
+                if (route === destination) {
+                    console.log(`BFS Found ${route} in ${steps} step(s)!`)
+                }
+                if (!visited.has(route)) {
+                    steps++
+                    visited.add(route)
+                    q.push(route)
+                    console.log(route)
+                }
+            }
+        }
+
+    }
+    findQuickestRoute(origin, destination, visited = new Set(), steps = 1) {
+        // DFS
+        const q = [origin]
+        visited.add(origin)
+        while (q.length > 0) {
+            const airport = q.pop(), routes = this.routes.get(airport)
+            for (const route of routes) {
+                if (route === destination) {
+                    console.log(`DFS Found ${route} in ${steps} step(s)!`)
+                }
+                if (!visited.has(route)) {
+                    console.log(route)
+                    this.findQuickestRoute(route, destination, visited, ++steps)
+                }
+            }
+        }
+    }
+}
+
+const airports = ['PHX','BKK','OKC','JFK','LAX','MEX','EZE','HEL','LOS','LAP','LIM']
 const routes = [
     ['PHX', 'LAX'],
     ['PHX', 'JFK'],
@@ -17,57 +68,8 @@ const routes = [
     ['LIM', 'BKK'],    
 ]
 
-const adjacencyList = new Map()
-
-function addNode(airport) {
-    adjacencyList.set(airport, [])
-}
-
-function addEdge(origin, destination) {
-    adjacencyList.get(origin).push(destination)
-    adjacencyList.get(destination).push(origin)
-}
-
-airports.forEach(addNode)
-routes.forEach(route => addEdge(...route))
-
-// breadthFirstSearch => All possible routes to determine which one is most efficient
-function breadthFirstSearch(startNode, endNode) {
-    const visited = new Set()
-    const queue = [startNode]
-    let steps = 1
-    while (queue.length > 0) {
-        const airport = queue.shift()
-        const destinations = adjacencyList.get(airport)
-        for (const destination of destinations) {
-            if (destination === endNode) {
-                console.log(`BFS Found ${endNode} in ${steps} step(s)!`)
-            }
-            if (!visited.has(destination)) {
-                steps++
-                visited.add(destination)
-                queue.push(destination)
-                console.log(destination)
-            }
-        }
-    }
-}
-breadthFirstSearch('PHX', 'BKK')
-
-// depthFirstSearch => Find Route where we don't care about shortest or best but as quickly as possible
-function depthFirstSearch(startNode, endNode, visited = new Set(), steps = 1) {
-    visited.add(startNode)
-    const destinations = adjacencyList.get(startNode)
-    for (const destination of destinations) {
-        if (destination === endNode) {
-            console.log(`DFS Found ${endNode} in ${steps} step(s)!`)
-            return
-        }
-        if (!visited.has(destination)) {
-            console.log(destination)
-            depthFirstSearch(destination, endNode, visited, ++steps)
-        }
-    }
-}
-
-depthFirstSearch('PHX', 'BKK')
+const rte = new Routes()
+airports.forEach(airport => rte.addNode(airport))
+routes.forEach(route => rte.addUndirectedEdge(...route))
+rte.findLeastHopsRoute('PHX', 'BKK')
+rte.findQuickestRoute('PHX', 'BKK')
